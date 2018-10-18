943ae97dd57de9f37397b1993248e5aba54d9d0c
Beanux <Beanux@users.noreply.github.com>
2018-10-18 15:04:00 +0200

    fix de l'authentification DHCP cf #3

--- genFibreID.sh
+++ genFibreID.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERNAME=$1
-AUTHSTRING=00:00:00:00:00:00:00:00:00:00:00:66:74:69:2f
+AUTHSTRING=00:00:00:00:00:00:00:00:00:00:00:1a:09:00:00:05:58:01:03:41:01:0d
 
 for (( i=0; i<${#USERNAME}; i++ )); do
         HEXCHAR=$(echo -n ${USERNAME:$i:1} | od -An -txC | xargs)
